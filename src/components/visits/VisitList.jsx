abd32a97083595d9b8e100859390dbfea3920afd
VitalyKolesnikov <vitaly.kolesnikov88@gmail.com>
2021-02-25 12:15:25 +0300

    fix favicon, add accordion to visitList

--- src/components/visits/VisitList.jsx
+++ src/components/visits/VisitList.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router'
 import { Link } from 'react-router-dom'
+import { Accordion, Card, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Cost from '../Cost'
 import VisitService from '../../services/VisitService'
 import makeInitials from '../../utils/makeInitials'
 
 export default function VisitList() {
-  const history = useHistory()
   const [visits, setVisits] = useState([])
 
   useEffect(() => {
@@ -18,46 +18,55 @@ export default function VisitList() {
   return (
     <div>
       <h2>Visits</h2>
-      <br></br>
-      {visits.map((visitsDto) => (
-        <div key={visitsDto.date}>
-          <h4>{visitsDto.date}</h4>
-          {visitsDto.visits.map((visit) => (
-            <table
-              className='table table-striped table-bordered table-sm'
-              key={visit.id}
+      {visits.map((visitsDto, idx) => (
+        <Accordion key={visitsDto.date} defaultActiveKey={1}>
+          <Card style={{ marginLeft: -20, marginRight: -15 }}>
+            <Accordion.Toggle
+              as={Card.Header}
+              variant='link'
+              eventKey={idx + 1}
             >
-              <tbody>
-                <tr>
-                  <td width='50%'>
-                    <Link to={'/visits/' + visit.id}>
-                      {visit.patient.lastName}{' '}
-                      {makeInitials(
-                        visit.patient.firstName,
-                        visit.patient.middleName
+              <h5>{visitsDto.date}</h5>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={idx + 1}>
+              <Card.Body className='col-xs-12'>
+                {visitsDto.visits.map((visit) => (
+                  <table
+                    className='table table-striped table-bordered table-sm'
+                    key={visit.id}
+                  >
+                    <tbody>
+                      <tr>
+                        <td width='50%'>
+                          <Link to={'/visits/' + visit.id}>
+                            {visit.patient.lastName}{' '}
+                            {makeInitials(
+                              visit.patient.firstName,
+                              visit.patient.middleName
+                            )}
+                          </Link>
+                        </td>
+                        <td>{visit.clinic.name}</td>
+                        <td width='20%'>
+                          <Cost value={visit.cost} />
+                        </td>
+                      </tr>
+                      {visit.info && (
+                        <tr>
+                          <td colSpan='3'>{visit.info}</td>
+                        </tr>
                       )}
-                    </Link>
-                  </td>
-                  <td>{visit.clinic.name}</td>
-                  <td width='20%'>
-                    <Cost value={visit.cost} />
-                  </td>
-                </tr>
-                {visit.info && (
-                  <tr>
-                    <td colSpan='3'>{visit.info}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          ))}
-          <span style={{ color: 'red' }}>
-            Total: <Cost value={visitsDto.totalSum} /> /{' '}
-            <Cost value={visitsDto.totalShare} /> руб.
-          </span>
-          <br></br>
-          <br></br>
-        </div>
+                    </tbody>
+                  </table>
+                ))}
+                <div style={{ color: 'red' }}>
+                  Total: <Cost value={visitsDto.totalSum} /> /{' '}
+                  <Cost value={visitsDto.totalShare} /> руб.
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       ))}
       <br></br>
     </div>
