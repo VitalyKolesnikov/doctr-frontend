275ea1d6b02dbcbb19dd97adf6c586d03d84da30
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2025-11-04 20:42:27 +0300

    removed "last 6 months"

--- src/components/visits/VisitList.jsx
+++ src/components/visits/VisitList.jsx
@@ -20,7 +20,7 @@ export default function VisitList() {
 
   return (
     <div>
-      <h2 style={{ paddingTop: 6 }}>Visits (last 6 months)</h2>
+      <h2 style={{ paddingTop: 6 }}>Visits</h2>
       {visits.map((visitsDto, idx) => (
         <Fragment key = {"acc_" + idx}>
         <Accordion key={visitsDto.date} defaultActiveKey={1}>
